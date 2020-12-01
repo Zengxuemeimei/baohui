72128b08088fc8df2d10cca9e46187c7bc0259ab
hq <1015766353@qq.com>
2020-12-01 13:52:55 +0800

    电子围栏，视频直播

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,10 +4,11 @@ const getters = {
   token: state => state.user.token,
   menu: state => state.user.menu,
   roles: state => state.user.roles,
+  userId: state => state.user.userId,
   name: state => state.user.name,
   enterpriseId: state => state.user.enterpriseId,
   permission_routes: state => state.permission.routes,
   visitedViews: state => state.tagsView.visitedViews,
-  cachedViews: state => state.tagsView.cachedViews 
+  cachedViews: state => state.tagsView.cachedViews
 }
 export default getters
